482ab4e2199d46b86c3c728ee8f6bdaed14d0b6b
HDLLakshan <HDLLakshan@users.noreply.github.com>
2020-04-04 14:47:10 +0530

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express();
 
 var port = process.env.PORT || 4000;
-app.get('/',(req,res) => res.send('Hellow OWrld'));
+app.get('/',(req,res) => res.send('Hellow Wrold I m lahiru lakshan'));
 
 app.listen(port, () => console.log('Server is running on port' + port))
