075791deaf4da29bdb5eaae277264e54672d8bc9
HDLLakshan <HDLLakshan@users.noreply.github.com>
2020-04-04 15:44:43 +0530

    change appjs 2

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cors());
+app.options('*', cors());
 app.use('/public', express.static('public'));
 app.use('/products', productRoute)
 
